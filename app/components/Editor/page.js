e69c774127a0ab4eb37a97022a1db1f0f33a33ea
Manan Rajput Chauhan <mananc12@gmail.com>
2024-06-23 00:24:37 +0530

    Update page.js

--- app/components/Editor/page.js
+++ app/components/Editor/page.js
@@ -1,22 +1,16 @@
 // components/Editor.js
-"use client"; // Ensure this is a client component
+export default function Editor(props) {
+    return (
+      <textarea
+        value={props.content}
+        onChange={(e) => props.onChange(e.target.value)}
+        className="w-full h-full p-2"
+      />
+    );
+  }
+  
+  
 
-import { useState } from 'react';
+  
 
-export default function Editor({ initialContent, onContentChange }) {
-  const [content, setContent] = useState(initialContent);
-
-  const handleChange = (e) => {
-    const newContent = e.target.value;
-    setContent(newContent);
-    onContentChange(newContent); // Notify parent component about the change
-  };
-
-  return (
-    <textarea
-      value={content}
-      onChange={handleChange}
-      className="w-full h-full p-2"
-    />
-  );
-}
+  
